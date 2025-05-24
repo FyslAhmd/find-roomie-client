f7dff19010bfd8e66d40495ecd20c470a9869f08
Faysal Ahmed <info.faysal.32@gmail.com>
2025-05-24 22:01:45 +0600

    responsive for mobile

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -6,7 +6,7 @@ import Footer from "../components/Footer";
 const Root = () => {
   const navigation = useNavigation();
   return (
-    <div className="w-11/12 mx-auto">
+    <div className="md:w-11/12 mx-auto">
       <Navbar></Navbar>
       {navigation.state === "loading" && (
         <div className="fixed inset-0 z-50 bg-base-100 bg-opacity-60 flex items-center justify-center">
